38903fca0d1d5aa129c3db71ee4ccc98dac10050
janeon <jhsieh@oberlin.edu>
2020-01-21 13:24:18 -0500

    gitignore and other updates

--- src/content.js
+++ src/content.js
@@ -1,22 +1,44 @@
 /*global chrome*/
-/* src/content.js */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Frame, { FrameContextConsumer }from 'react-frame-component';
+import Frame, { FrameContextConsumer } from 'react-frame-component';
 import "./content.css";
 
 class Main extends React.Component {
+  constructor(props) {
+      super(props);
+      var title = document.getElementById('post-title').childNodes[1].childNodes[3].childNodes[1];
+      var editor = document.getElementById('post-editor').childNodes[1].childNodes[3].childNodes[3].childNodes[1].childNodes[1].value
+      title.focus();
+
+      if (/```.*?```/gus.exec(editor)) {
+        this.snippet = " Woohoo you've included a code snippet!"
+      }
+      else {
+        this.snippet = " Hey, could you possibly add some code for context?"
+      }
+
+      if (editor.length < 1350) {;
+        this.length = " The body's a bit short, give some more description perhaps? "
+      } else {
+        this.length += " Now that's a good amount of content "
+      }
+  }
+
     render() {
         return (
-            <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}> 
+            <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}>
                <FrameContextConsumer>
                {
-               // Callback is invoked with iframe's window and document instances
                    ({document, window}) => {
-                      // Render Children
                         return (
                            <div className={'my-extension'}>
-                               <h1>Hello world - My first Extension</h1>
+                               <h2>Construction Tips</h2>
+                               <div>{this.snippet}</div>
+                               <div></div>
+                               <div>{this.length}</div>
+                               <h2>Similar Questions</h2>
+                               Click anywhere to load questions
                            </div>
                         )
                     }
